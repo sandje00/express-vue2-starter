0a015ef5e2a8ab713022523bf14aaea4ff208106
sandje00 <stella.andjelic.00@fesb.hr>
2021-04-05 12:09:26 +0200

    Add logger

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const helmet = require('helmet');
+const { middleware: logRequests } = require('./shared/logger');
 const { port } = require('./config');
 const router = require('./router');
 
@@ -12,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
-app.use('/api/v1', router);
+app.use('/api/v1', logRequests, router);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
---
+++ server/shared/logger.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const bunyan = require('bunyan');
+
+const logLevel = (() => {
+  if (process.env.LOG_LEVEL) return process.env.LOG_LEVEL;
+  return process.env.NODE_ENV === 'production' ? 'info' : 'debug';
+})();
+
+const logger = bunyan.createLogger({
+  name: 'app-logger',
+  serializers: bunyan.stdSerializers,
+  level: logLevel
+});
+
+const middleware = (req, res, next) => {
+  logger.info({ req });
+  next();
+};
+
+module.exports = { logger, middleware };
