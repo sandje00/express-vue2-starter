a8cd93c5a870f0f6ea440a2944fcbaa16902fbfa
sandje00 <stella.andjelic.00@fesb.hr>
2021-04-08 18:20:09 +0200

    Add base field component

---
+++ client/components/common/BaseField.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot><base-input v-on="$listeners" v-bind="$attrs" /></slot>
+  </div>
+</template>
+
+<script>
+import BaseInput from './BaseInput';
+
+export default {
+  name: 'base-field',
+  inheritAttrs: false,
+  components: { BaseInput }
+};
+</script>
