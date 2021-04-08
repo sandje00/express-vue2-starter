b6c79c23383ad129085eeb998d015936801c9b97
sandje00 <stella.andjelic.00@fesb.hr>
2021-04-08 18:39:05 +0200

    Add base button component

---
+++ client/components/common/BaseButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button v-on="$listeners">
+    <span>{{ text }}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'base-button',
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
