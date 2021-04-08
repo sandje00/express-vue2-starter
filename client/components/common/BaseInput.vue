d1ef3a747dc9024c8ada24af1338dbbc91956f3f
sandje00 <stella.andjelic.00@fesb.hr>
2021-04-08 18:12:39 +0200

    Add base input component

---
+++ client/components/common/BaseInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input
+    v-on="listeners"
+    @input="emit('input', $event.target.value)"
+    @change="emit('change', $event.target.value)"
+    :value="value"
+    class="input">
+</template>
+
+<script>
+export default {
+  name: 'base-input',
+  props: {
+    value: { type: String, default: '' }
+  },
+  computed: {
+    listeners() {
+      const { input, change, ...listeners } = this.$listeners;
+      return listeners;
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
