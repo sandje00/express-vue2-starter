31f063fa131c1a0b84ea40c187076065950c9190
sandje00 <stella.andjelic.00@fesb.hr>
2021-04-10 19:58:00 +0200

    Add register API call

--- client/components/Register.vue
+++ client/components/Register.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div v-if="successMessage">{{ successMessage }}</div>
-    <form v-else>
+    <form
+      v-else
+      @submit.prevent="handleSubmit(register)">
       <base-field
         v-model="username"
         type="text"
@@ -27,6 +29,8 @@
 <script>
 import BaseButton from './common/BaseButton';
 import BaseField from './common/BaseField';
+import pick from 'lodash/pick';
+import usersApi from '../api/users';
 
 export default {
   name: 'register-view',
@@ -38,6 +42,18 @@ export default {
     successMessage: '',
     error: ''
   }),
+  methods: {
+    register() {
+      usersApi
+        .register(pick(this, ['username', 'email', 'password']))
+        .then(({ data: { message } }) => {
+          this.successMessage = message;
+        })
+        .catch(({ status, data: { error } }) => {
+          this.error = status === 400 ? error : 'Something went wrong. Try again.';
+        });
+    }
+  },
   components: { BaseButton, BaseField }
 };
 </script>
