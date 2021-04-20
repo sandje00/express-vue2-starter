b3ddd1cff159e0c73e6ac6e514e837696e870121
sandje00 <stella.andjelic.00@fesb.hr>
2021-04-20 21:52:12 +0200

    Add base checkbox

---
+++ client/components/common/BaseCheckbox.vue
@@ -0,0 +1,24 @@
+<template>
+  <input
+    v-on="listeners"
+    @input="$emit('input', $event.target.value)"
+    @change="$emit('change', $event.target.value)"
+    :checked="value"
+    type="checkbox"
+    class="input">
+</template>
+
+<script>
+export default {
+  name: 'base-checkbox',
+  props: {
+    value: { type: Boolean, default: false }
+  },
+  computed: {
+    listeners() {
+      const { input, change, ...listeners } = this.$listeners;
+      return listeners;
+    }
+  }
+};
+</script>
