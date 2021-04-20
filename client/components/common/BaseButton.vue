c18af3809cd5c97d9d035b4460d95c04a1f306e4
sandje00 <stella.andjelic.00@fesb.hr>
2021-04-20 21:43:55 +0200

    Apply style to base button

--- client/components/common/BaseButton.vue
+++ client/components/common/BaseButton.vue
@@ -1,6 +1,9 @@
 <template>
-  <button v-on="$listeners">
-    <span>{{ text }}</span>
+  <button v-on="$listeners" :class="{ primary, rounded }" class="button py-s px-xl">
+    <div class="flex-h justify-center align-center">
+      <span v-if="icon" :class="icon" class="icon mr-s"></span>
+      <span class="text">{{ text }}</span>
+    </div>
   </button>
 </template>
 
@@ -8,6 +11,18 @@
 export default {
   name: 'base-button',
   props: {
+    primary: {
+      type: Boolean,
+      default: false
+    },
+    rounded: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
     text: {
       type: String,
       required: true
@@ -15,3 +30,42 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.button {
+  font-size: 1rem;
+  background: none;
+  border: none;
+  cursor: pointer;
+
+  .icon, .text {
+    color: var(--color-grey-light-700);
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:disabled {
+    background-color: var(--color-grey-light-900);
+    pointer-events: none;
+    cursor: default;
+  }
+}
+
+.rounded {
+  border-radius: 50px;
+}
+
+.primary {
+  background-color: var(--color-primary);
+
+  &:hover {
+    background-color: var(--color-primary-light-100);
+  }
+
+  .icon, .text {
+    color: var(--color-white);
+  }
+}
+</style>
