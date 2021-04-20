c6e6c6c6f2e2490350d8d7f72296854dd4fae0aa
sandje00 <stella.andjelic.00@fesb.hr>
2021-04-20 21:56:15 +0200

    Add base select

---
+++ client/components/common/BaseSelect.vue
@@ -0,0 +1,43 @@
+<template>
+  <select
+    v-on="listeners"
+    @input="$emit('input', $event.target.value)"
+    :value="value"
+    class="base-select pa-s">
+    <option
+      v-for="{ label, id } in options"
+      :key="label"
+      :value="id">
+      {{ label }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'base-select',
+  props: {
+    options: { type: Array, required: true },
+    value: { type: String, required: true }
+  },
+  computed: {
+    listeners() {
+      const { input, ...listeners } = this.$listeners;
+      return listeners;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.base-select {
+  width: 100%;
+  color: var(--color-grey-dark-300);
+  background: var(--color-grey-light-900);
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
