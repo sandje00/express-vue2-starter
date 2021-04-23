9d1691f1f88d2de058fd1aa8b9a8591a4a4237b9
sandje00 <stella.andjelic.00@fesb.hr>
2021-04-23 21:37:48 +0200

    Add form field and base form components

---
+++ client/components/common/BaseForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <validation-observer v-slot="{ handleSubmit }">
+    <form
+      @submit.prevent="handleSubmit(submit)">
+      <slot></slot>
+    </form>
+  </validation-observer>
+</template>
+
+<script>
+export default {
+  name: 'base-form',
+  methods: {
+    submit() {
+      this.$emit('submit');
+    }
+  }
+};
+</script>
---
+++ client/components/common/FormField.vue
@@ -0,0 +1,23 @@
+<template>
+  <validation-provider
+    v-slot="{ errors }"
+    :name="name"
+    :rules="rules"
+    class="mt-l">
+    <base-field
+      v-model="firstName"
+      :error="errors[0]"
+      type="text"
+      :placeholder="name" />
+  </validation-provider>
+</template>
+
+<script>
+export default {
+  name: 'form-field',
+  props: {
+    name: { type: String, required: true },
+    rules: { type: [String, Object], required: true }
+  }
+};
+</script>
