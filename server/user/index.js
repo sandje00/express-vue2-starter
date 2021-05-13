cad3d2dfe6b9bb4c862d807302469497a0052d34
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-13 17:47:41 +0200

    Rewrite verify email function

--- server/user/index.js
+++ server/user/index.js
@@ -2,10 +2,11 @@
 
 const ctrl = require('./user.controller');
 const router = require('express').Router();
+const verify = require('../shared/auth/verify');
 
 router
   .post('/register', ctrl.register)
-  .get('/verify/:token', ctrl.verify)
+  .get('/verify/:token', verify, ctrl.verify)
   .post('/login', ctrl.login);
 
 module.exports = { path: '/users', router };
--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const { auth: { secret } } = require('../config');
 const Audience = require('../shared/auth/audience');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const { UniqueConstraintError } = require('sequelize');
 const User = require('./user.model');
 
@@ -25,38 +23,20 @@ async function register({ body }, res) {
   });
 }
 
-async function verify(req, res) {
-  const { token } = req.params;
-
-  try {
-    const { id, aud } = jwt.verify(token, secret);
-    if (aud !== Audience.Scope.Setup) {
-      return res.status(400).send({
-        message: 'Token is invalid'
-      });
-    }
-
-    const user = await User.findByPk(id);
-    if (!user) {
-      return res.status(404).send({
-        message: 'We are unable to find user for this token'
-      });
-    }
-    if (user.active) {
-      return res.status(409).send({
-        message: 'This account has already been verified'
-      });
-    }
-    user.active = true;
-    await user.save();
-  } catch (err) {
-    if (err instanceof jwt.TokenExpiredError) {
-      return res.status(400).send({
-        message: 'Token is expired'
-      });
-    }
-    throw err;
+async function verify({ id }, res) {
+  const user = await User.findByPk(id);
+  if (!user) {
+    return res.status(404).send({
+      message: 'We are unable to find user for this token'
+    });
   }
+  if (user.active) {
+    return res.status(409).send({
+      message: 'This account has already been verified'
+    });
+  }
+  user.active = true;
+  await user.save();
 
   return res.status(200).send({
     message: 'Your email account has been verified'
