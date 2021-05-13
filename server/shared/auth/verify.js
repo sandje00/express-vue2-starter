cdc59f4d1cb41712055c637c138e52ddd1344067
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-13 17:39:19 +0200

    Extract messages to variable

--- server/shared/auth/verify.js
+++ server/shared/auth/verify.js
@@ -4,28 +4,25 @@ const { auth: { secret } } = require('../../config');
 const Audience = require('./audience');
 const jwt = require('jsonwebtoken');
 
+const TOKEN_INVALID = 'Token is invalid';
+const TOKEN_EXPIRED = 'Token is expired';
+
 function verify(req, res, next) {
   const { token } = req.body;
 
   try {
     const { id, aud } = jwt.verify(token, secret);
     if (aud !== Audience.Scope.Setup) {
-      return res.status(400).send({
-        message: 'Token is invalid'
-      });
+      return res.status(400).send({ message: TOKEN_INVALID });
     }
     req.id = id;
     next();
   } catch (err) {
     if (err instanceof jwt.JsonWebTokenError) {
-      return res.status(400).send({
-        message: 'Token is invalid'
-      });
+      return res.status(400).send({ message: TOKEN_INVALID });
     }
     if (err instanceof jwt.TokenExpiredError) {
-      return res.status(400).send({
-        message: 'Token is expired'
-      });
+      return res.status(400).send({ message: TOKEN_EXPIRED });
     }
     throw err;
   }
