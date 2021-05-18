595fc712b607c040b1fe3b2e4f4231fdc1de4898
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-18 22:32:20 +0200

    Configure authentication middleware

---
+++ server/shared/auth/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const { ExtractJwt, Strategy: JwtStrategy } = require('passport-jwt');
+const Audience = require('./audience');
+const { auth: { secret } } = require('../../config');
+const passport = require('passport');
+const { User } = require('../database');
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secret,
+  audience: Audience.Scope.Setup
+};
+
+passport.use(new JwtStrategy(jwtOptions, (payload, done) => {
+  return User.findByPk(payload.id)
+    .then(user => done(null, user || false))
+    .catch(err => done(err, false));
+}));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+module.exports = {
+  initialize(options) {
+    return passport.initialize(options);
+  },
+  authenticate(strategy, options = {}) {
+    return passport.authenticate(strategy, options);
+  }
+};
