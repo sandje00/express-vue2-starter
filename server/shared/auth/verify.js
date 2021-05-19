fa5e9b905703665abe07b1c6ea19c3e400ed3fc1
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-19 19:52:48 +0200

    Fix jwt verification middleware

--- server/shared/auth/verify.js
+++ server/shared/auth/verify.js
@@ -8,7 +8,7 @@ const TOKEN_INVALID = 'Token is invalid';
 const TOKEN_EXPIRED = 'Token is expired';
 
 function verify(req, res, next) {
-  const { token } = req.body;
+  const token = req.params.token || req.body.token;
   try {
     const { id, aud } = jwt.verify(token, secret);
     if (aud !== Audience.Scope.Setup) {
