3cbe8592a4d9e5c4082b3d25178f7f2b504a8bc6
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-19 21:36:41 +0200

    Add global error handler

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const database = require('./shared/database');
 const express = require('express');
 const helmet = require('helmet');
+const { StatusCodes: INTERNAL_SERVER_ERROR } = require('http-status-codes');
 const { middleware: logRequests } = require('./shared/logger');
 const { port } = require('./config');
 const router = require('./router');
@@ -19,5 +20,14 @@ app.use('/api/v1', logRequests, router);
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+app.use(errorHandler);
+
+function errorHandler(err, req, res, next) {
+  return err.status
+    ? res.status(err.status).json({ error: err.message })
+    : res.status(INTERNAL_SERVER_ERROR)
+      .json({ error: 'Something went wrong' });
+}
 
 database.initialize();
