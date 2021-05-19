e32a0dd99d36557ec2273c4ffb68fca80beba59e
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-19 19:29:08 +0200

    Destructure token from request body

--- server/shared/auth/verify.js
+++ server/shared/auth/verify.js
@@ -8,8 +8,7 @@ const TOKEN_INVALID = 'Token is invalid';
 const TOKEN_EXPIRED = 'Token is expired';
 
 function verify(req, res, next) {
-  const { token } = req.params || req.body;
-
+  const { token } = req.body;
   try {
     const { id, aud } = jwt.verify(token, secret);
     if (aud !== Audience.Scope.Setup) {
