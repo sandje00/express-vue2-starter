634c453969d6a59616016ff98f980b852b4154e7
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-21 15:31:26 +0200

    Remove redundant variable

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -98,8 +98,7 @@ class User extends Model {
   }
 
   async passwordCompare(password) {
-    const result = await bcrypt.compare(password, this.password);
-    return result;
+    return bcrypt.compare(password, this.password);
   }
 
   createToken(options = {}) {
