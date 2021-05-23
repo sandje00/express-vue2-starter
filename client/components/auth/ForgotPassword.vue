f13c636164e2384196163c66f8908e7402a6a2a2
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-23 18:43:11 +0200

    Add forgot password form

---
+++ client/components/auth/ForgotPassword.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div v-if="message">{{ message }}</div>
+    <base-form>
+      <base-field
+        v-model.trim="email"
+        name="E-mail"
+        :rules="{ required: true, email: true }"
+        type="text"
+        placeholder="E-mail" />
+      <base-button
+        type="submit"
+        text="Send password reset e-mail"
+        class="btn mt-m"
+        primary
+        rounded />
+    </base-form>
+  </div>
+</template>
+
+<script>
+import BaseButton from '../shared/BaseButton';
+import BaseField from '../shared/BaseField';
+import BaseForm from '../shared/BaseForm';
+
+export default {
+  name: 'forgot-password',
+  data: () => ({
+    email: '',
+    message: ''
+  }),
+  components: { BaseButton, BaseField, BaseForm }
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  align-self: center;
+}
+</style>
