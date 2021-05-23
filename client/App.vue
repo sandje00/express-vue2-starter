04706b6e44c13c867de426f4b22d12c98c10d279
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-23 16:28:42 +0200

    Remove unnecessary component

--- client/App.vue
+++ client/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app">
-    <router-link to="/register">Register</router-link>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <hello-world :msg="message" />
     <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 export default {
-  name: 'app-main',
-  data: () => ({ message: 'Hello World!' }),
-  components: {
-    HelloWorld
-  }
+  name: 'app-main'
 };
 </script>
 
