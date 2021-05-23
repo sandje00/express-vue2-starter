df14c03ccf789f61407661b7eadb1c70e8d9ab0a
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-23 19:03:22 +0200

    Add reset password form

---
+++ client/components/auth/ResetPassword.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div v-if="message">{{ message }}</div>
+    <base-form>
+      <base-field
+        v-model="password"
+        name="Password"
+        :rules="{ required: true, min: { length: 8 } }"
+        type="password"
+        placeholder="New password" />
+      <base-field
+        v-model="repeat"
+        name="Repeat password"
+        :rules="{ required: true, confirmation: { target: '@Password' } }"
+        type="password"
+        placeholder="Repeat password" />
+      <base-button
+        type="submit"
+        text="Send password reset e-mail"
+        class="btn mt-m"
+        primary
+        rounded />
+    </base-form>
+  </div>
+</template>
+
+<script>
+import BaseButton from '../shared/BaseButton';
+import BaseField from '../shared/BaseField';
+import BaseForm from '../shared/BaseForm';
+
+export default {
+  name: 'forgot-password',
+  data: () => ({
+    password: '',
+    repeat: '',
+    token: '',
+    error: '',
+    message: ''
+  }),
+  components: { BaseButton, BaseField, BaseForm }
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  align-self: center;
+}
+</style>
