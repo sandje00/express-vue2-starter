331c239da2f209eb0a5940540a487c2e4509c2de
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-23 18:31:39 +0200

    Add verify email view

---
+++ client/components/auth/Verify.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex-v justify-center align-center mb-m px-s">
+    <h3 class="mt-l">Click on the button below to verify your email account</h3>
+    <span v-if="message" class="msg mt-xl">{{ message }}</span>
+    <base-button
+      v-else
+      class="mt-xl"
+      text="Verify your account"
+      primary
+      rounded />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'verify-email',
+  props: {
+    token: { type: String, required: true }
+  },
+  data: () => ({ message: '' })
+};
+</script>
+
+<style lang="scss" scoped>
+.msg {
+  font-size: 1.5rem;
+}
+</style>
--- client/router.js
+++ client/router.js
@@ -2,6 +2,7 @@ import Auth from './components/auth';
 import Home from './components/Home';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Verify from './components/auth/Verify';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
@@ -14,7 +15,8 @@ const routes = [
     component: Auth,
     children: [
       { path: 'register', name: 'register', component: Register },
-      { path: 'login', name: 'login', component: Login }
+      { path: 'login', name: 'login', component: Login },
+      { path: 'verify/:token', component: Verify, props: true }
     ]
   }
 ];
