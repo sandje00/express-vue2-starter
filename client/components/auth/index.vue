aeb1d63caaf7f8e7308294eb15926c0d93d0eea9
sandje00 <stella.andjelic.00@fesb.hr>
2021-05-23 16:26:02 +0200

    Add auth view

---
+++ client/components/auth/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main flex-v justify-center align-center">
+    <div class="container flex-v justify-center align-center pb-m">
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main {
+  height: 100vh;
+  background: var(--color-background);
+
+  .container {
+    width: 100%;
+    max-width: var(--measure-l);
+    align-items: stretch;
+    min-height: var(--spc-m);
+    margin: 0 auto;
+    background: var(--color-background-auth-form);
+    box-shadow: -4px 0 24px 13px rgba(0,0,0,0.05);
+  }
+}
+</style>
